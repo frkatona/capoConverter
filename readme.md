970ffb146173a7d0c27820d517a1b0e02c3eb55c
frkatona <apk5101@psu.edu>
2023-06-18 11:00:33 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Input chords and output valid capo positions
+valid format:
+ - capital letters alone for major chords (e.g., A major -> A)
+ - capital letters with a lower case 'm' for minor chords (e.g., A minor -> Am)
+ - separated by spaces (e.g., A Dm C Fm)
+
+# to-do
+ - allow user to enter current capo position and adjust accordingly
+ - add additional chord types
+ - convert to ipynb or subpage of website
---
+++ testbench.py
@@ -0,0 +1,20 @@
+capoPositions = {
+    '0': ['A', 'Am', 'C', 'D', 'Dm', 'E', 'Em', 'F', 'Fm', 'G'],
+    '1': ['A#', 'A#m', 'C#', 'D#', 'D#m', 'F', 'Fm', 'F#', 'F#m', 'G#'],
+    '2': ['B', 'Bm', 'D', 'E', 'Em', 'F#', 'F#m', 'G', 'Gm', 'A'],
+    '3': ['C', 'Cm', 'D#', 'F', 'Fm', 'G', 'Gm', 'G#', 'G#m', 'A#'],
+    '4': ['C#', 'C#m', 'E', 'F#', 'F#m', 'G#', 'G#m', 'A', 'Am', 'B'],
+    '5': ['D', 'Dm', 'F', 'G', 'Gm', 'A', 'Am', 'A#', 'A#m', 'C'],
+    '6': ['D#', 'D#m', 'F#', 'G#', 'G#m', 'A#', 'A#m', 'B', 'Bm', 'C#'],
+    '7': ['E', 'Em', 'G', 'A', 'Am', 'B', 'Bm', 'C', 'Cm', 'D'],
+    '8': ['F', 'Fm', 'G#', 'A#', 'A#m', 'C', 'Cm', 'C#', 'C#m', 'D#'],
+    '9': ['F#', 'F#m', 'A', 'B', 'Bm', 'C#', 'C#m', 'D', 'Dm', 'E'],
+    '10': ['G', 'Gm', 'A#', 'C', 'Cm', 'D', 'Dm', 'D#', 'D#m', 'F'],
+    '11': ['G#', 'G#m', 'B', 'C#', 'C#m', 'D#', 'D#m', 'E', 'Em', 'F#']
+}
+
+chordList = ['A', 'C']
+
+for position in capoPositions:
+    if all(elem in position for elem in chordList):
+        print(position)
